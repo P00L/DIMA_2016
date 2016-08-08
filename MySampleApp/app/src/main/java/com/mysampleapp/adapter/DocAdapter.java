44669141924eb8b88fdb5953fa3fc255f6b6edee
P00L <fusari.pool@gmail.com>
2016-08-08 16:04:42 +0200

    added doctor adapter

---
+++ MySampleApp/app/src/main/java/com/mysampleapp/adapter/DocAdapter.java
@@ -0,0 +1,94 @@
+package com.mysampleapp.adapter;
+
+import android.content.Context;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mysampleapp.R;
+import com.mysampleapp.demo.nosql.DoctorDO;
+import com.mysampleapp.demo.nosql.DrugDO;
+import com.mysampleapp.fragment.DocFragment;
+import com.mysampleapp.fragment.DrugFragment;
+
+public class DocAdapter extends RecyclerView.Adapter<DocAdapter.ViewHolder> {
+
+    private Context mContext;
+    private DoctorDO[] mList;
+
+    public DocAdapter(Context contexts, DoctorDO[] list) {
+        this.mContext = contexts;
+        this.mList = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.doc_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.titleTextView.setText(mList[position].getName());
+        holder.setClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                if (isLongClick) {
+                    Toast.makeText(mContext, "#" + position + " - " + mList[position] + " (Long click)", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, "#" + position + " - " + mList[position], Toast.LENGTH_SHORT).show();
+                    DocFragment fragment = DocFragment.newInstance();
+                    fragment.setResult(mList[position]);
+                    AppCompatActivity activity = (AppCompatActivity) mContext;
+                    activity.getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.content_frame, fragment)
+                            .addToBackStack(null)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                            .commit();
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener{
+        private TextView titleTextView;
+        private ItemClickListener clickListener;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTextView = (TextView)itemView.findViewById(R.id.text_name);
+            itemView.setTag(itemView);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+
+        }
+
+        public void setClickListener(ItemClickListener itemClickListener) {
+            this.clickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            clickListener.onClick(view, getPosition(), false);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            clickListener.onClick(view, getPosition(), true);
+            return true;
+        }
+    }
+}
